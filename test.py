51c2f0cd8d4e197e464aa5b94e13de48e4303257
OmarArriaga <omar@nightingalesecurity.com>
2017-08-12 03:01:13 -0700

    Updated code to change from all motors to individual motors

--- test.py
+++ test.py
@@ -1,56 +1,19 @@
 #!/usr/bin/env python
 
-import time
 
 import explorerhat
+from time import sleep
+from random import randint
 
-
-print("""
-This basic example tests the various functions of Explorer HAT,
-touching any of the buttons should output a message, lights should pulse
-and analog/digital values should be read every second.
-
-Press CTRL+C to exit.
-""")
-
-explorerhat.motor.forwards()
-
-touched = [False] * 8
-
-
-def ohai(channel, event):
-    touched[channel - 1] = True
-    print("{}: {}".format(channel, event))
-
-
-explorerhat.touch.pressed(ohai)
-explorerhat.touch.released(ohai)
-
-explorerhat.light[0].on()
-explorerhat.light[2].on()
-
-explorerhat.output[0].on()
-explorerhat.output[2].on()
-
-input_status = False
-
-while True:
-
-    if explorerhat.is_explorer_pro():
-        print(explorerhat.analog.read())
-    print(explorerhat.input.read())
-    if sum(explorerhat.input.read().values()) == 4:
-        input_status = True
-    print('Input Status:', input_status)
-    print('Touch Status:', sum(touched) == 8)
-
-    if input_status and sum(touched) == 8:
-        explorerhat.light.off()
-        explorerhat.light.green.on()
-    else:
-        explorerhat.light.toggle()
-    explorerhat.output.toggle()
-
-    time.sleep(1)
-
-explorerhat.pause()
+try:
+    while True:
+        duration = randint(1,5)
+        explorerhat.motor.one.forward(20)
+        explorerhat.motor.two.forward(20)
+        sleep(duration)
+        explorerhat.motor.one.backward(20)
+        explorerhat.motor.two.forward(20)
+        sleep(duration)
+except KeyboardInterrupt:
+    explorerhat.motor.stop()
+    explorerhat.output.one.off()
